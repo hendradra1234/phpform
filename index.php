732bee952fb7d0d36d0c3dadfb557b5711368c84
hendra-hp-victus <hendra@artamedianet.co.id>
2024-07-20 08:26:32 +0700

    init project

---
+++ index.php
@@ -0,0 +1,28 @@
+<html>
+    <head>
+        <title>Tugas 1</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+    </head>
+    <body>
+        <?php
+            echo "<h3><strong>Running php in date "
+            . date("Y-m-d")
+            . "<br></strong></h3>";
+        ?>
+        <h1>UTS DPW</h1>
+        <h2>List UTS DPW Semester 2</h2>
+        <p>
+            <label>List Tugas 1</label>
+            <ul>
+                <li><a href="modules/percabangan/">Percabangan</a></li>
+                <li><a href="modules/perualangan/">Perulangan</a></li>
+                <li><a href="modules/formArray">Form Array</a></li>
+                <li><a href="modules/formOutput/">Form Output</a></li>
+            </ul>
+        </p>
+    </body>
+</html>
+
---
+++ modules/formArray/index.php
@@ -0,0 +1,25 @@
+<html>
+    <head>
+        <title>UTS DPW Pengolahan Form - Form Array</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+    </head>
+    <body>
+        <h1>Form Array</h1>
+        <form action="" method="POST" name="input">
+            <label>Nama Anda</label>
+            <div class = "form-group">
+                <input type="text" name="nama" placeholder = "Masukkan nama anda"/>
+            </div>
+            <button type="submit" name="Input" value="Input">Submit</button>
+        </form>
+    </body>
+</html>
+<?php
+    if (isset($_POST["Input"])) {
+        $nama= $_POST["nama"];
+        echo "Nama Anda : <b>$nama</b>";
+    }
+?>
